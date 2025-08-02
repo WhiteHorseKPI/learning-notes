80007292e63a8a23b6233d20f62c4995653e4406
WhiteHorseKPI <robin14good@gmail.com>
2025-08-02 15:53:46 +0200

    feat: init project

---
+++ src/main/kotlin/com/learningnotes/service/LearningNotesApplication.kt
@@ -0,0 +1,11 @@
+package com.learningnotes.service
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LearningNotesApplication
+
+fun main(args: Array<String>) {
+	runApplication<LearningNotesApplication>(*args)
+}
