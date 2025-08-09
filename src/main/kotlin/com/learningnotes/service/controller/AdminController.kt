9343ca5931055baad55eda14cc648e777877df79
WhiteHorseKPI <robin14good@gmail.com>
2025-08-09 20:54:35 +0200

    feat: add API to promote student to teacher

--- src/main/kotlin/com/learningnotes/service/controller/AdminController.kt
+++ src/main/kotlin/com/learningnotes/service/controller/AdminController.kt
@@ -4,11 +4,9 @@ import com.learningnotes.service.response.UserResponse
 import com.learningnotes.service.service.AdminService
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
+import jakarta.validation.constraints.Min
 import org.springframework.http.HttpStatus
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.ResponseStatus
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 
 @Tag(name = "Admin REST API Endpoints", description = "Operations related to a admin")
@@ -21,5 +19,12 @@ class AdminController(private val adminService: AdminService) {
     @Operation(summary = "Get all users", description = "Retrieve a list of all users in the system")
     fun getAllUsers(): List<UserResponse> {
         return adminService.getAllUsers()
+    }
+
+    @Operation(summary = "Promote user to admin", description = "Promote user to teacher role")
+    @ResponseStatus(HttpStatus.OK)
+    @PutMapping("/{userId}/role")
+    fun promoteToAdmin(@PathVariable @Min(1) userId: Long): UserResponse {
+        return adminService.promoteToAdmin(userId)
     }
 }
--- src/main/kotlin/com/learningnotes/service/service/AdminService.kt
+++ src/main/kotlin/com/learningnotes/service/service/AdminService.kt
@@ -1,9 +1,14 @@
 package com.learningnotes.service.service
 
+import com.learningnotes.service.entity.Authority
+import com.learningnotes.service.entity.User
 import com.learningnotes.service.repository.UserRepository
 import com.learningnotes.service.response.UserResponse
+import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
+import org.springframework.web.server.ResponseStatusException
+import java.util.*
 
 
 @Service
@@ -14,5 +19,26 @@ class AdminService(private val userRepository: UserRepository) {
             .findAll()
             .map { UserResponse.fromUser(it) }
             .toList()
+    }
+
+    @Transactional
+    fun promoteToAdmin(userId: Long): UserResponse {
+        val user: Optional<User> = userRepository.findById(userId)
+        if (user.isEmpty || user.get().authorities!!
+                .stream()
+                .anyMatch { ("ROLE_TEACHER" == it.authority)
+            }) {
+            throw ResponseStatusException(
+                HttpStatus.BAD_REQUEST,
+                "User does not exist or already an teacher"
+            )
+        }
+        val authorities: MutableList<Authority> = mutableListOf()
+        authorities.add(Authority("ROLE_STUDENT"))
+        authorities.add(Authority("ROLE_TEACHER"))
+        val notNullUser = user.get()
+        notNullUser.authorities = authorities
+        val savedUser: User = userRepository.save(notNullUser)
+        return UserResponse.fromUser(savedUser)
     }
 }
